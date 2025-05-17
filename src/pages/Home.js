4e0552920831aad637251209ca7d23535b90187a
akesbihajar <akesbihajar1@gmail.com>
2025-05-17 20:01:49 +0200

    Your message here

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
-import BannerImage from "../assets/bgg.jpg";
 import "../Styles/Home.css";
+import VideoBg from "../assets/video.mp4"; // remplace par ton nom de fichier
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-      <div className="headerContainer">
-        <h1 className="title">ZAHARA RIAD</h1>
-        
-      </div>
+    <div className="home">
+      {/* Vidéo en arrière-plan */}
+      <video autoPlay muted loop className="background-video">
+        <source src={VideoBg} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+     
     </div>
   );
 }
